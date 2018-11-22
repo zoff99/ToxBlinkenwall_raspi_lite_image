b659e7a047797fa9578c4323401675b5812f76b9
Zoff <zoff99@users.noreply.github.com>
2018-11-22 21:48:27 +0100

    Create 02-run.sh

---
+++ stage2/04-toxblinkenwall/02-run.sh
@@ -0,0 +1,33 @@
+#!/bin/bash -e
+
+on_chroot << EOF
+
+# disable swap
+service dphys-swapfile stop
+sudo systemctl disable dphys-swapfile
+apt-get --yes --force-yes purge dphys-swapfile
+
+# backup alsa config
+cp -av /usr/share/alsa/alsa.conf /usr/share/alsa/alsa.conf_ORIG
+
+# enable imagemagick to read things from files
+cp -av /etc/ImageMagick-6/policy.xml /etc/ImageMagick-6/policy.xml.BACKUP
+sed -i -e 's#^.*<policy domain="path".*$#<!-- removed by ToxBlinkenwall -->#g' /etc/ImageMagick-6/policy.xml
+
+# add tbw to rc.local
+sed -i -e 's#exit 0##' /etc/rc.local
+printf '\n' >> /etc/rc.local
+printf 'echo none > /sys/class/leds/led0/trigger\n' >> /etc/rc.local
+printf 'su - pi bash -c "/home/pi/ToxBlinkenwall/toxblinkenwall/initscript.sh start" > /dev/null 2>/dev/null &\n' >> /etc/rc.local
+printf '\n' >> /etc/rc.local
+printf 'exit 0\n' >> /etc/rc.local
+
+EOF
+
+echo
+echo
+# just check the files contents
+cat "${ROOTFS_DIR}/etc/rc.local"
+echo
+echo
+
