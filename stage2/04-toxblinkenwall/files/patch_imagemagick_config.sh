95b697c4ba8929dab359caae7525584be8111a14
Zoff <zoff99@users.noreply.github.com>
2018-11-24 09:28:10 +0100

    Update patch_imagemagick_config.sh

--- stage2/04-toxblinkenwall/files/patch_imagemagick_config.sh
+++ stage2/04-toxblinkenwall/files/patch_imagemagick_config.sh
@@ -3,3 +3,7 @@
 echo "patching ImageMagick config ..."
 sed -i -e 's#^.*<policy domain="path".*$#<!-- removed by ToxBlinkenwall -->#g' "${ROOTFS_DIR}/etc/ImageMagick-6/policy.xml"
 echo "... ready"
+echo "---------------------------------"
+echo "changes in ImageMagick config:"
+diff "${ROOTFS_DIR}/etc/ImageMagick-6/policy.xml.BACKUP" "${ROOTFS_DIR}/etc/ImageMagick-6/policy.xml"
+echo "---------------------------------"
