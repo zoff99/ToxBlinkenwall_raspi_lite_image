44b8b95431f8ccd05dd4434cd8deae4c625065f9
zoff99 <zoff@zoff.cc>
2019-01-12 09:18:49 +0100

    wrong path

--- stage2/04-toxblinkenwall/01-run.sh
+++ stage2/04-toxblinkenwall/01-run.sh
@@ -28,7 +28,7 @@ cp -av /etc/ImageMagick-6/policy.xml /etc/ImageMagick-6/policy.xml.BACKUP
 echo "configure rc.local"
 sed -i -e 's#exit 0##' /etc/rc.local
 printf '\n' >> /etc/rc.local
-printf '(sleep 5;/home/pi/detect_usb_audio.sh) &\n' >> /etc/rc.local
+printf '(sleep 5;/home/pi/ToxBlinkenwall/toxblinkenwall/detect_usb_audio.sh) &\n' >> /etc/rc.local
 printf '\n' >> /etc/rc.local
 printf 'bash /set_random_passwds.sh > /dev/null 2>/dev/null &\n' >> /etc/rc.local
 printf '\n' >> /etc/rc.local
