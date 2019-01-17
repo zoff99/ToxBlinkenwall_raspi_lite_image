0febe4e839896b5af1607cab635c8aee31ce0bf7
zoff99 <zoff@zoff.cc>
2019-01-17 09:34:47 +0100

    export pi password to paired usb device for debug builds, add more parameters to udev scripts

--- stage2/04-toxblinkenwall/files/set_random_passwds.sh
+++ stage2/04-toxblinkenwall/files/set_random_passwds.sh
@@ -3,6 +3,8 @@
 if [ -e /_first_start_ ]; then
     rand_pass=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9\_\%' | fold -w 30 | head -n 1)
     echo "pi:$rand_pass" | chpasswd
+    echo "$rand_pass" > /etc/pipa.txt
+    chmod og-rwx /etc/pipa.txt
 
     rand_pass=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9\_\%' | fold -w 30 | head -n 1)
     echo "root:$rand_pass" | chpasswd
