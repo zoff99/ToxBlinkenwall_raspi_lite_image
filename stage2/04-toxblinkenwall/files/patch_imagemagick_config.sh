a781b4f7fd3a0248d13a3e003810257323276c49
zoff99 <zoff@zoff.cc>
2019-05-16 19:25:05 +0200

    cleanup

--- stage2/04-toxblinkenwall/files/patch_imagemagick_config.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-echo "patching ImageMagick config ..."
-sed -i -e 's#^.*<policy domain="path".*$#<!-- removed by ToxBlinkenwall -->#g' "${ROOTFS_DIR}/etc/ImageMagick-6/policy.xml"
-echo "... ready"
-echo "---------------------------------"
-echo "changes in ImageMagick config:"
-diff "${ROOTFS_DIR}/etc/ImageMagick-6/policy.xml.BACKUP" "${ROOTFS_DIR}/etc/ImageMagick-6/policy.xml"
-echo "---------------------------------"
